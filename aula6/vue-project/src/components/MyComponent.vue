9bf4e4496821bb1756decd7f30abf58265eb3dd6
trevisharp <leonardotrevisansilio@gmail.com>
2024-12-09 16:05:09 -0300

    add MyComponent.vue

---
+++ aula6/vue-project/src/components/MyComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps([ 'text' ]);
+</script>
+
+<template>
+    <marquee>
+        <h1>
+            {{ text }}
+        </h1>
+    </marquee>
+</template>
+
+<style scoped>
+
+</style>
